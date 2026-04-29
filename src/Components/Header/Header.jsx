b3ba8a85fe34223589ea17e1aee3166360235737
Adepu Sanjay <adepusanjay812@gmail.com>
2026-04-29 13:51:21 +0530

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,37 +3,45 @@ import './Header.css';
 
 function Header() {
   return (
-    <div className="saas-doc-header">
-      <div className="saas-doc-header__left">
-        <span className="saas-doc-badge">COLLEGE CODE: K9</span>
-        <p className="saas-doc-text">
-          Survey No.8/A, Medbowli, Meerpet<br />
-          Hyderabad - 500097, Telangana, INDIA
-        </p>
-        <a href="http://www.tkrct.ac.in" className="saas-doc-link">
-          www.tkrct.ac.in
-        </a>
-      </div>
+    <footer className="saas-footer-block">
+      <div className="saas-footer-block__inner">
+        
+        {/* Left Column: Branding */}
+        <div className="saas-footer-block__brand">
+          <img src="./images/logo.png" alt="TKRCET Logo" className="saas-footer-block__logo" />
+          <div>
+            <h2 className="saas-footer-block__title">
+              TKR COLLEGE OF ENGINEERING & TECHNOLOGY
+            </h2>
+            <span className="saas-footer-block__highlight">An Autonomous Institution</span>
+          </div>
+        </div>
 
-      <div className="saas-doc-header__center">
-        <img src="./images/logo.png" alt="TKRCET Logo" className="saas-doc-logo" />
-        <h1 className="saas-doc-title">
-          TKR COLLEGE OF ENGINEERING & TECHNOLOGY
-        </h1>
-      </div>
+        {/* Center Column: Contact & Location */}
+        <div className="saas-footer-block__info">
+          <span className="saas-footer-block__badge">COLLEGE CODE: K9</span>
+          <p className="saas-footer-block__text">
+            Survey No.8/A, Medbowli, Meerpet<br />
+            Hyderabad - 500097, Telangana, INDIA
+          </p>
+          <a href="http://www.tkrct.ac.in" className="saas-footer-block__link">
+            www.tkrct.ac.in
+          </a>
+        </div>
 
-      <div className="saas-doc-header__right">
-        <span className="saas-doc-highlight">An Autonomous Institution</span>
-        <p className="saas-doc-text">
-          Accredited with 'A+' Grade by NAAC<br />
-          Approved by AICTE, New Delhi<br />
-          Affiliated to JNTUH<br />
-          Recognized under 2(f) & 12(B) of UGC
-        </p>
+        {/* Right Column: Accreditation */}
+        <div className="saas-footer-block__accreditation">
+          <p className="saas-footer-block__text">
+            Accredited with 'A+' Grade by NAAC<br />
+            Approved by AICTE, New Delhi<br />
+            Affiliated to JNTUH<br />
+            Recognized under 2(f) & 12(B) of UGC
+          </p>
+        </div>
+
       </div>
-    </div>
+    </footer>
   );
 }
 
 export default Header;
- 
