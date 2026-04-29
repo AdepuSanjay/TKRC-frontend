c5553c43b769cb3c36fc5d852e925315ece8d33b
Adepu Sanjay <adepusanjay812@gmail.com>
2026-04-29 09:17:47 +0530

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,21 +4,27 @@ import './Header.css';
 function Header() {
   return (
     <div className="header-container">
+      
       <div className="info-left">
         <p className="college-code">COLLEGE CODE: K9</p>
         <p className="address">
-          Survey No.8/A, Medbowli, Meerpet<br />Hyderabad - 500097, Telangana, INDIA
+          Survey No.8/A, Medbowli, Meerpet<br />
+          Hyderabad - 500097, Telangana, INDIA
         </p>
         <p>
-          <a href="http://www.tkrct.ac.in" className="website-link">www.tkrct.ac.in</a>
+          <a href="http://www.tkrct.ac.in" className="website-link" target="_blank" rel="noopener noreferrer">
+            www.tkrct.ac.in
+          </a>
         </p>
       </div>
+
       <div className="content-center">
+        <img src="./images/logo.png" alt="TKRCET Logo" className="college-logo" />
         <h1 className="college-name">
           TKR COLLEGE OF ENGINEERING & TECHNOLOGY
         </h1>
-        <img src="./images/logo.png" alt="TKRCET Logo" className="college-logo" />
       </div>
+
       <div className="info-right">
         <p className="autonomous-text">An Autonomous Institution</p>
         <p className="accreditation-info">
@@ -28,6 +34,7 @@ function Header() {
           Recognized under 2(f) & 12(B) of UGC
         </p>
       </div>
+
     </div>
   );
 }
